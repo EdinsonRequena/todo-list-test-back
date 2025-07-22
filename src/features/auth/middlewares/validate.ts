30bb3f70ad070f22d36a9d4bb8863f666836c38c
edinsonrequena <edinsonrequena@protonmail.com>
2025-07-22 00:53:43 -0400

    Refactor: standardize code formatting in authentication middleware and schemas

--- src/features/auth/middlewares/validate.ts
+++ src/features/auth/middlewares/validate.ts
@@ -2,9 +2,9 @@ import { ObjectSchema } from 'joi';
 import { Request, Response, NextFunction } from 'express';
 
 export const validate =
-    (schema: ObjectSchema) => (req: Request, _res: Response, next: NextFunction) => {
-        const { error, value } = schema.validate(req.body, { abortEarly: false });
-        if (error) return next({ status: 400, message: error.message });
-        req.body = value;
-        next();
-    };
+  (schema: ObjectSchema) => (req: Request, _res: Response, next: NextFunction) => {
+    const { error, value } = schema.validate(req.body, { abortEarly: false });
+    if (error) return next({ status: 400, message: error.message });
+    req.body = value;
+    next();
+  };
--- src/features/auth/schemas.ts
+++ src/features/auth/schemas.ts
@@ -1,11 +1,11 @@
 import Joi from 'joi';
 
 export const registerSchema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().min(6).max(50).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).max(50).required(),
 });
 
 export const loginSchema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
 });
